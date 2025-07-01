6bfd16d8ef11a18167e654258a3ac4b0e37d1035
sneka.22 <snekag@centizen.com>
2025-07-01 22:24:02 +0530

    ssr remove changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { SkillsetComponent } from './skillset/skillset.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,AboutmeComponent,SkillsetComponent],
+  imports: [AboutmeComponent,SkillsetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
